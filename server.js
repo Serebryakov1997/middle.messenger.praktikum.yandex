61faac389e57de79ad2773cb20d74157f6ffee18
Serebryakov1997 <ewan5erebryakov@yandex.ru>
2023-06-21 14:42:15 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+
+
+const app = express();
+const PORT = 3000;
+
+const currentDir = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(currentDir);
+
+app.use(express.static(__dirname + '/build'));
+console.log('dirname: ', __dirname + '/build');
+
+app.listen(PORT, function () {
+    console.log(`My Web Messenger listening on port ${PORT}!`);
+});
