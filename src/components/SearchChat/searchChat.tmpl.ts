d9cbab8b8ac4b0a57bb2856a4c59b155b43e4caa
Serebryakov1997 <ewan5erebryakov@yandex.ru>
2023-08-16 01:56:32 -0400

    add start code for searchChat page

---
+++ src/components/SearchChat/searchChat.tmpl.ts
@@ -0,0 +1,4 @@
+export const searchChatTmpl = `
+    <div class="{{styles.seachChatClass}}">
+    </div>
+`;
---
+++ src/pages/SelectChat/selectChat.tmpl.ts
@@ -0,0 +1,5 @@
+export const selectChatTmpl = `
+    <form>
+        {{{searchComponent}}}
+    </form>
+`;
