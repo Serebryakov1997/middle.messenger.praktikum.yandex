b3bb5ca4c185dd58a51b7bbf81a19805810eab7d
Serebryakov1997 <ewan5erebryakov@yandex.ru>
2023-10-21 16:40:54 -0400

    complete profile_change_passwd page and fix some styles

--- src/views/pages/profile/profile.ts
+++ src/views/pages/profile/profile.ts
@@ -1,23 +1,28 @@
 import './profile.css';
-import { Block, DEV_LINK_ADDRESS } from '../../../utils';
+import { Block, DEV_LINK_ADDRESS, inputValidation } from '../../../utils';
 import { profileTmpl } from './profile.tmpl';
-import { ProfileField } from '../../components';
+import { Input, Label, ValidError } from '../../components';
+import { emailValidator } from '../../../models/validators';
 
-const ProfileFieldStyles = {
-    profileFieldClass: 'profile-field',
-    labelClass: 'profile-label',
-    inputClass: 'profile-input',
-    profileFieldType: 'text'
+
+const mockData = {
+    email: 'ivanivanov@yandex.ru',
+    login: 'ivanivanov',
+    first_name: 'Иван',
+    second_name: 'Иванов',
+    chat_name: 'Иван',
+    phone: '+79099673030'
 }
 
 
 export class Profile extends Block {
+
+    _formData: FormData;
+
     constructor() {
         super('form', {
             styles: {
                 containerClass: 'profile-container',
-                // cameraClass: 'camera-button',
-                // buttonType: 'button',
                 displayNameClass: 'display-name',
                 statusClass: 'status',
                 changeDataClass: 'change-data',
@@ -35,12 +40,47 @@ export class Profile extends Block {
             toLoginLink: DEV_LINK_ADDRESS,
             logoutName: 'Выйти'
         });
+        this._formData = new FormData();
+        Object.entries(mockData).forEach(([key, value]) => {
+            this._formData.set(key, value);
+        });
     }
 
 
     protected init(): void {
         this.children = {
-            // labelEmail: 
+            labelEmail: new Label({
+                name: 'email',
+                labelName: 'Почта',
+                styles: {
+                    labelClass: 'profile-label'
+                }
+            }),
+            inputEmail: new Input({
+                name: 'email',
+                validErrorId: 'error',
+                styles: {
+                    inputProfileClass: 'profile-input'
+                },
+                inputType: 'text',
+                inputValue: mockData.email,
+                events: {
+                    blur: (e: Event) => {
+                        this._formData.set('email', (<HTMLInputElement>e.target).value);
+                        inputValidation(e, emailValidator, {
+                            validError: <Block>this.children.validErrorEmail,
+                            input: <Block>this.children.inputEmail,
+                            button: <Block>this.children.buttonSave
+                        });
+                    }
+                }
+            }),
+            validErrorEmail: new ValidError('div', {
+                styles: {
+                    validErrClass: 'valid-err'
+                },
+                validErrorId: 'error'
+            }),
         }
     }
 
