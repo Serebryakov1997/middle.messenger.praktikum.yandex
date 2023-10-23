82b549b918f728596c7da8aa0ac8324b1aac87a2
Serebryakov1997 <ewan5erebryakov@yandex.ru>
2023-10-23 08:41:32 -0400

    add scrolling for register form

--- src/views/pages/register/register.tmpl.ts
+++ src/views/pages/register/register.tmpl.ts
@@ -2,33 +2,35 @@ export const registerTmpl = `
     <form action="{{buttonLink}}">
         <div class="{{styles.registerContainerClass}}">
             <div class="{{styles.registerHeaderClass}}">{{registerHeaderName}}</div>
-            {{{labelEmail}}}
-            {{{inputEmail}}}
-            {{{validErrorEmail}}}
-    
-            {{{labelLogin}}}
-            {{{inputLogin}}}
-            {{{validErrorLogin}}}
-    
-            {{{labelName}}}
-            {{{inputName}}}
-            {{{validErrorName}}}
-    
-            {{{labelSurName}}}
-            {{{inputSurName}}}
-            {{{validErrorSurName}}}
-    
-            {{{labelPhone}}}
-            {{{inputPhone}}}
-            {{{validErrorPhone}}}
-    
-            {{{labelPasswd}}}
-            {{{inputPasswd}}}
-            {{{validErrorPasswd}}}
-    
-            {{{labelRepeatPasswd}}}
-            {{{inputRepeatPasswd}}}
-            {{{validErrorRepeatPasswd}}}
+            <div class="{{styles.registerPayloadClass}}">
+                {{{labelEmail}}}
+                {{{inputEmail}}}
+                {{{validErrorEmail}}}
+        
+                {{{labelLogin}}}
+                {{{inputLogin}}}
+                {{{validErrorLogin}}}
+        
+                {{{labelName}}}
+                {{{inputName}}}
+                {{{validErrorName}}}
+        
+                {{{labelSurName}}}
+                {{{inputSurName}}}
+                {{{validErrorSurName}}}
+        
+                {{{labelPhone}}}
+                {{{inputPhone}}}
+                {{{validErrorPhone}}}
+        
+                {{{labelPasswd}}}
+                {{{inputPasswd}}}
+                {{{validErrorPasswd}}}
+        
+                {{{labelRepeatPasswd}}}
+                {{{inputRepeatPasswd}}}
+                {{{validErrorRepeatPasswd}}}
+            </div>
 
             {{{registerButton}}}
             {{{underButtonLink}}}
